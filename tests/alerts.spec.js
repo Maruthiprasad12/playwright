94eb46bceb57d3d40e707ebe7af079b050f9b9dd
Maruthiprasad12 <pcmaruthiprasad01@gmail.com>
2025-07-04 13:13:49 +0530

    playwright script

--- tests/alerts.spec.js
+++ tests/alerts.spec.js
@@ -15,4 +15,37 @@ test.describe("working with Alerts-popups", () => {
         await page.locator('//div[@class="example"]/ul/li/button[text()="Click for JS Alert"]').click();
         await page.waitForTimeout(1000);
     });
+    test("working with confirm alert", async ({ page }) => {
+        page.on("dialog", async (dialog) => {
+            expect(dialog.type()).toBe("confirm");
+            expect(dialog.message()).toBe("I am a JS Confirm");
+            await dialog.accept();
+        });
+        await page.getByText("Click for JS Confirm").click();
+    });
+    test("working with cancell-confirm alert", async ({ page }) => {
+        page.on("dialog", async (dialog) => {
+            expect(dialog.type()).toBe("confirm");
+            expect(dialog.message()).toBe("I am a JS Confirm");
+            await dialog.dismiss();
+        });
+        await page.getByText("Click for JS Confirm").click();
+    });
+    test("working with accept-prompt alert", async ({ page }) => {
+        page.on("dialog", async (dialog) => {
+            expect(dialog.type()).toBe("prompt");
+            expect(dialog.message()).toBe("I am a JS prompt");
+            await dialog.accept("Maruthi");
+        });
+        await page.locator('button[onclick="jsPrompt()"]').click();
+    });
+    test("working with cancel   -prompt alert", async ({ page }) => {
+        page.on("dialog", async (dialog) => {
+            expect(dialog.type()).toBe("prompt");
+            expect(dialog.message()).toBe("I am a JS prompt");
+            await dialog.dismiss();
+        });
+        await page.locator('button[onclick="jsPrompt()"]').click();
+    });
+
 });
---
+++ tests/texts.spec.js
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+test.describe("working with text element", async () => {
+    // //    test.beforeEach(async ({ page }) => {
+    //         await page.goto("https://www.amazon.in/")
+    //     })
+    test('working with texts', async ({ page }) => {
+        await expect(page.locator('(//a[@class="nav-a  "])[6]')).toHaveText('Mobiles')
+        const textvalue = await page.locator('(//a[@class="nav-a  "])[9]').textContent()
+        console.log(textvalue)
+
+    })
+    test('workig with all text content', async ({ page }) => {
+        await page.goto('https://www.flipkart.com/')
+        // const values = await page.locator('//a[@class="_1ch8e_"]/div/div/span/span').allTextContents()
+        // for (let i of values) {
+        //     console.log(i)
+        // }
+        const textvalue = await page.locator('(//a[@class="_1ch8e_"]/div/div/span/span)[3]').innerText()
+        console.log(textvalue)
+        const values = await page.locator('//a[@class="_1ch8e_"]/div/div/span/span').allInnerTexts()
+        for (let i of values) {
+            console.log(i)
+        }
+
+    })
+})
